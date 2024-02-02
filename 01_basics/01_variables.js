30e13bd5dca13d346425d2cdfd3835516aadba93
Saraswan Chakrabortty <csaraswan@gmail.com>
2024-02-02 18:21:24 +0530

    Three files added

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail = "csaraswan@gmail.com"
+var accountPassword = "12345"
+accountCity = "Chennai"
+
+// accountId = 2
+
+accountEmail = "r@h.com"
+accountPassword = "22121"
+accountCity = "Kolkata"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("hello")  // we are useing node not browser
+
+let name1 = "Saraswan"
+let age = 18
+let isLoggedin = false
+let state
+
+// number => 2 to the power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => it is not nothing. it is a standalone value
+// undefined => undefined is just undefined. it is not null
+// symbol => used for unique
+
+// object
+
+console.log(typeof state);
+console.log(typeof age);
+console.log(typeof name1);
+
+console.log(typeof null);  // WTF 😑 This thing is a object
+console.log(typeof undefined);
+
+
