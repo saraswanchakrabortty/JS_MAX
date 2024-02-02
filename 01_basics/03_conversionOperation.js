58e345a34e4be990e3938f955863910c7dcee66c
Saraswan Chakrabortty <csaraswan@gmail.com>
2024-02-02 19:48:53 +0530

    New FIle

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,29 @@
+// let score = "33"
+
+// let strange = null
+
+// let strange2 = "33add"
+
+// let valuInNumber = Number(score)
+
+// console.log(typeof score);
+
+// console.log(typeof(valuInNumber));
+
+// let valuInNumber2 = Number(strange)
+
+// console.log(typeof(valuInNumber2));
+
+// console.log(valuInNumber2);
+
+// let valuInNumber3 = Number(strange2)
+
+// console.log(typeof(valuInNumber3));
+
+// console.log(valuInNumber3);
+
+// "33" => 33
+// "33abs" => NaN  // Convertion not possible that is why it's NaN(Not a number) but it's a type number😅
+
+// true => 1
+// false => 0
