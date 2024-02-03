0116ced5e485e783e31374ed086564a5cc2316b9
Saraswan Chakrabortty <csaraswan@gmail.com>
2024-02-03 23:15:39 +0530

    Strings and Math files added

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,14 @@
+const name = "Saraswan"
+const repoCount = 50
+
+// console.log(name+repoCount+" Value"); //<-- Old Method
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+
+const newGame = new String('Saraswan')  //<<-- Declare String using javascript object
+
+console.log(newGame[0]);
+console.log(newGame.__proto__);
+
+console.log(newGame);
---
+++ 01_basics/06_num_and_maths.js
@@ -0,0 +1,9 @@
+const score = 400
+
+const balace = new Number(100)
+
+
+console.log(score);
+console.log(balace);
+
+
