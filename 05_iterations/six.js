d79f89954a0815de9c7d663022cd09af019e6984
Saraswan Chakrabortty <csaraswan@gmail.com>
2024-02-28 18:16:21 +0530

    Fixed File Structure

--- 05_iterations/six.js
+++ 05_iterations/six.js
@@ -8,4 +8,4 @@ const langs = ["Rust", "GOLang", "TypeScript", "JavaScript"]
 
 const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-const newNums = myNums.filter( (num) => num > 4 
+const newNums = myNums.filter( (num) => num > 4 )
