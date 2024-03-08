fcb00669821096b2d4cce3f7438a11309bbac730
Saraswan Chakrabortty <94130787+saraswanchakrabortty@users.noreply.github.com>
2024-03-08 20:18:22 +0530

    Formatting file names

--- 09_advanced_one/promises.js
+++ 09_advanced_one/promises.js
@@ -16,23 +16,26 @@
 
 
 
-promiseOne = new Promise(function(resolve, reject){
-    // Do an async task
-    // DB calls, cryptography, network
-    setTimeout(function(){
-        console.log('Async task 2 is complete.')
-        resolve()
-    }, 1000)
+// promiseOne = new Promise(function(resolve, reject){
+//     // Do an async task
+//     // DB calls, cryptography, network
+//     setTimeout(function(){
+//         console.log('Async task 2 is complete.')
+//         resolve()
+//     }, 1000)
 
-}).then(function(){
-    console.log("Promise Consumed");
-})
-
-
+// }).then(function(){
+//     console.log("Promise Consumed");
+// })
 
 
 
-
-// const promiseThree = new Promise(function(resolve, reject)){
-
-// }
+// const promiseThree = new Promise(function(resolve, reject){
+//     setTimeout(function(){
+//         resolve({username: "me", email: "me@example.com"})
+//     },1000)
+// })
+                                                        //<<- A promise with passing value with resolve
+// promiseThree.then(function(user){
+//     console.log(user);
+// })
